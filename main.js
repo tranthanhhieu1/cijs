40cfd130afc66cce14b27b5ad495134f624344e8
DESKTOP-NF3LVTF\ASUS <tranthanhhieu180299@gmail.com>
2021-04-21 18:12:10 +0700

    Homework_1

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const navItems = [
     {
         name:"Item 1",
+        children:["Item 1.1", "Item 1.2", "Item 1.3"]
     },
     {
         name:"Item 2",
+        children:[
+            "Item 1.1",
+            "Item 1.2",
+            "Item 1.3",
+            "Item 1.1",
+            "Item 1.2",
+            "Item 1.3"
+        ]
     },
     {
         name:"Item 3",
@@ -17,8 +26,19 @@ const navItems = [
 ];
 
 window.onload = function(){
+    let i = 0;
     for(let a of navItems){
         document.getElementsByClassName("menu")[0].innerHTML +=`
-            <span>` + a.name + `</span>`;
+            <span>` + a.name + `
+                <div class="menu-block"></div>
+            </span>`;
+            if(a.children){
+                for(let b of a.children){
+                    document.getElementsByClassName("menu-block")[i].innerHTML +=`
+                        <span>`+ b + `</span>
+                    `;
+                }
+                i++;
+            }
     }
 }
